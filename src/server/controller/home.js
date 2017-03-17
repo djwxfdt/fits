32582a34fba865099b511ba23cea0d1a842a2f3d
Sampson <dujianwxfdt@live.cn>
2017-03-17 15:08:32 +0800

    article

--- src/server/controller/home.js
+++ src/server/controller/home.js
@@ -1,7 +1,9 @@
 const userInfo = require('../../test/datas/user.json')
+const articles = require('../../test/datas/articles.json')
 
 
 module.exports = (req,res)=>{
 	res.locals.user = userInfo
+	res.locals.articles = articles
 	res.render(`theme/${userInfo.theme || 'default'}`)
 }
