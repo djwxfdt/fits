398855683ae33fa68b67c2c2763e5e1bd8075858
Sampson <dujianwxfdt@live.cn>
2017-03-30 14:52:28 +0800

    public

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const setupPolicy = require('./server/policy.js')
 global.appRoot = path.resolve(__dirname)
 
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'pug')
 
 i18n.configure({
