1018657a01b6ba76bde0496bdc88faa68cb7eafa
Sampson <dujianwxfdt@live.cn>
2017-04-14 17:12:54 +0800

    convert

--- src/server/controller/home.js
+++ src/server/controller/home.js
@@ -1,10 +1,15 @@
 const userInfo = require('../../test/datas/user.json')
 const Post = require('../service/post.js')
+const showdown = require('showdown')
 
 module.exports.index = (req,res)=>{
 	res.locals.user = userInfo
+	let converter = new showdown.Converter()
 	Post.all().then(list=>{
-		res.locals.articles = list
+		res.locals.articles = list.map(item=>{
+			 item.body = converter.makeHtml(item.body)
+			 return item
+		})
 		res.render(`theme/${userInfo.theme || 'default'}/index`)
 	})
 }
